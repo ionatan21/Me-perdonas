f67512620f66d220a5b4a87251c81dd7826ea8e6
UCR <barriosjonatan1221@gmail.com>
2024-06-29 12:43:33 -0600

    me_perdonas

---
+++ src/LandingPage.jsx
@@ -0,0 +1,72 @@
+// src/components/LandingPage.jsx
+import React, { useState, useEffect } from "react";
+import "./LandingPage.css";
+
+const messages = [
+  "tengo algo importante que decir... 😔",
+  "Lo siento mucho 😭",
+  "No volverá a pasar 🙏",
+  "¿Me perdonas 🥺?",
+];
+
+const LandingPage = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [noClickCount, setNoClickCount] = useState(0);
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentMessageIndex((prevIndex) => {
+        if (prevIndex < messages.length - 1) {
+          return prevIndex + 1;
+        } else {
+          clearInterval(interval);
+          return prevIndex;
+        }
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleYesClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleNoClick = () => {
+    setNoClickCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div className="landing-page">
+      {messages[currentMessageIndex] && (
+        <h1 className={`title${currentMessageIndex}`}>{messages[currentMessageIndex]}</h1>
+      )}
+
+      {currentMessageIndex === messages.length - 1 ? (
+        <div className="buttons">
+          <button
+            className="yes-button"
+            style={{ transform: `scale(${1 + noClickCount * 0.1})` }}
+            onClick={handleYesClick}
+          >
+            Sí
+          </button>
+          <button className="no-button" onClick={handleNoClick}>
+            No
+          </button>
+        </div>
+      ) : null}
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <h2>¡Sabía que lo harías! tmk ❤️</h2>
+            <img src="https://res.cloudinary.com/ds8cp2nxp/image/upload/v1719686169/yufla2dmzsta1_z86rfi.gif" alt="Background" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LandingPage;
